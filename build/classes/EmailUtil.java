573bd63858eb9b42820caf5d5c9dc6919ca45562
Sahil Srivastava <32928561+sahilhbti@users.noreply.github.com>
2018-11-02 05:37:48 +0530

    Add files via upload

---
+++ build/classes/EmailUtil.java
@@ -0,0 +1,20 @@
+
+import javax.mail.Session;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc
+ */
+class EmailUtil {
+
+    static void sendEmail(Session session, String toEmail, String tlsEmail_Testing_Subject, String tlsEmail_Testing_Body) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
